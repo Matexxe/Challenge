36cc18a89d0134c9cd7e95d29babe1c7ef9820ec
Matexxe <158209261+Matexxe@users.noreply.github.com>
2024-01-30 14:39:13 -0500

    Add files via upload

---
+++ encriptador.js
@@ -0,0 +1,92 @@
+const btnEncriptar = document.querySelector(".btn-encriptador");
+const txtEncriptar = document.querySelector(".encriptar");
+const aviso = document.querySelector(".texto-aviso");
+const respuesta = document.querySelector(".evaluar");
+const contenido = document.querySelector(".tarjeta-contenedor");
+const btnCopiar = document.querySelector(".btn-copiar");
+const btnDesencriptar = document.querySelector(".btn-desencriptador");
+
+//-------Boton de Encriptar-------//
+btnEncriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+    
+    if(texto == ""){
+    swal("Error","El campo de texto no debe estar vacio", "error");
+  }
+
+    else if(texto !== txt){
+        swal( "Error","No debe tener acentos ni caracteres especiales","error");
+        txtEncriptar.value = "";
+        
+    }
+
+    else if(texto !== texto.toLowerCase()){
+       swal("Error", "El texto debe ser todo en minuscula", "error");
+       txtEncriptar.value = "";
+    }
+
+    else{
+        texto = texto.replace(/e/mg, "enter");
+        texto = texto.replace(/i/mg, "imes");
+        texto = texto.replace(/a/mg, "ai");
+        texto = texto.replace(/o/mg, "ober");
+        texto = texto.replace(/u/mg, "ufat");
+
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove(); 
+        txtEncriptar.value = "";
+        swal("Bien", "Texto encriptado con exito", "success");
+    }
+});
+
+//-------Boton de Desencriptar-------//
+btnDesencriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    swal("Bien", "Has desencriptado con exito", "success");
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, "");
+    
+    if(texto == ""){
+         swal("Error", "El campo de texto no debe estar vacio", "error");
+        
+        
+    }
+
+    else if(texto !== txt){
+         swal("Error", "No debe tener acentos y caracteres especiales", "error");
+         txtEncriptar.value = "";
+        
+        
+    }
+
+    else if(texto !== texto.toLowerCase()){
+        swal("Error", "El texto debe ser todo en miniscula", "error");
+        txtEncriptar.value = "";
+    }
+
+    else{
+        texto = texto.replace(/enter/mg, "e");
+        texto = texto.replace(/imes/mg, "i");
+        texto = texto.replace(/ai/mg, "a");
+        texto = texto.replace(/ober/mg, "o");
+        texto = texto.replace(/ufat/mg, "u");
+
+        respuesta.innerText = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove(); 
+        txtEncriptar.value = "";
+       
+    }
+});
+
+//-------Boton de Copiar-------//
+btnCopiar.addEventListener("click", e=>{
+    e.preventDefault();
+    let copiar = respuesta;
+    copiar.select();
+    document.execCommand("copy"); 
+    swal("Bien", "Texto copiado en el portapapeles", "success");
+});
